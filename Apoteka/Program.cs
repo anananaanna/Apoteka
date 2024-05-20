7c1e45e33b53f36e54718fbd22bf2734b1143352
Anastasija <anastasija.jevtic@elfak.rs>
2024-05-20 15:58:30 +0200

    Poruka

---
+++ Apoteka/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Apoteka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
